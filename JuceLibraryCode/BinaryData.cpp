4b50754972d2a5e390d7c54f36e79720a5a43b9a
AUTO64 <info@auto64>
2026-01-19 23:37:13 +1100

    Phase 8: compiles

---
+++ JuceLibraryCode/BinaryData.cpp
@@ -0,0 +1,126 @@
+/* ==================================== JUCER_BINARY_RESOURCE ====================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+#include <cstring>
+
+namespace BinaryData
+{
+
+//================== delete.svg ==================
+static const unsigned char temp_binary_data_0[] =
+"<?xml version=\"1.0\" encoding=\"utf-8\"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->\r\n"
+"<svg width=\"800px\" height=\"800px\" viewBox=\"0 0 16 16\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\r\n"
+"<path d=\"M5.1716 8.00003L1.08582 3.91424L3.91424 1.08582L8.00003 5.1716L12.0858 1.08582L14.9142 3.91424L10.8285 8.00003L14.9142 12.0858L12.0858 14.9142L8.00003 10.8285L3.91424 14.9142L1.08582 12.0858L5.1716 8.00003Z\" fill=\"#000000\"/>\r\n"
+"</svg>";
+
+const char* delete_svg = (const char*) temp_binary_data_0;
+
+//================== duplicate.svg ==================
+static const unsigned char temp_binary_data_1[] =
+"<?xml version=\"1.0\" encoding=\"utf-8\"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->\r\n"
+"<svg width=\"800px\" height=\"800px\" viewBox=\"0 0 16 16\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\r\n"
+"<path d=\"M11 0H5V12H15V4L11 0Z\" fill=\"#000000\"/>\r\n"
+"<path d=\"M1 4V16H11V14H3V4H1Z\" fill=\"#000000\"/>\r\n"
+"</svg>";
+
+const char* duplicate_svg = (const char*) temp_binary_data_1;
+
+//================== lock.svg ==================
+static const unsigned char temp_binary_data_2[] =
+"<?xml version=\"1.0\" encoding=\"utf-8\"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->\r\n"
+"<svg width=\"800px\" height=\"800px\" viewBox=\"0 0 16 16\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\r\n"
+"<path fill-rule=\"evenodd\" clip-rule=\"evenodd\" d=\"M16 8C16 12.4183 12.4183 16 8 16C3.58172 16 0 12.4183 0 8C0 3.58172 3.58172 0 8 0C12.4183 0 16 3.58172 16 8ZM10.3128 12.4341C9.62116 12.7956 8.83445 13 8 13C5.23858 13 3 10.7614 3 8C3 7.16555 3.20"
+"441 6.37884 3.5659 5.68722L10.3128 12.4341ZM12.4341 10.3128L5.68722 3.5659C6.37884 3.20441 7.16555 3 8 3C10.7614 3 13 5.23858 13 8C13 8.83445 12.7956 9.62116 12.4341 10.3128Z\" fill=\"#000000\"/>\r\n"
+"</svg>";
+
+const char* lock_svg = (const char*) temp_binary_data_2;
+
+//================== regen.svg ==================
+static const unsigned char temp_binary_data_3[] =
+"<?xml version=\"1.0\" encoding=\"utf-8\"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->\r\n"
+"<svg width=\"800px\" height=\"800px\" viewBox=\"0 0 16 16\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\r\n"
+"<path d=\"M5.68623 0H10.3138L12.178 3.27835L13.9282 2.26789L14.4282 3.13392L13.3301 7.232L9.23203 6.13392L8.73203 5.26789L10.4459 4.27837L9.15033 2L6.84966 2L6.29552 2.97447L4.56343 1.97445L5.68623 0Z\" fill=\"#000000\"/>\r\n"
+"<path d=\"M13.1649 9.05964L13.7039 10.0076L12.6055 12H9.99998L9.99998 9.99995H8.99998L5.99998 12.9999L8.99998 15.9999H9.99998L9.99998 14H13.7868L15.996 9.99242L14.8969 8.05962L13.1649 9.05964Z\" fill=\"#000000\"/>\r\n"
+"<path d=\"M3.39445 12H4.49998V14H2.21325L0.00390625 9.99242L1.8446 6.75554L0.0717772 5.732L0.571776 4.86598L4.66986 3.7679L5.76793 7.86598L5.26793 8.732L3.57669 7.75556L2.29605 10.0076L3.39445 12Z\" fill=\"#000000\"/>\r\n"
+"</svg>";
+
+const char* regen_svg = (const char*) temp_binary_data_3;
+
+//================== reverse.svg ==================
+static const unsigned char temp_binary_data_4[] =
+"<?xml version=\"1.0\" encoding=\"utf-8\"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->\r\n"
+"<svg width=\"800px\" height=\"800px\" viewBox=\"0 0 16 16\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\r\n"
+"<path d=\"M6 14H8L8 9L13 14H15L15 2H13L8 7L8 2H6L0 8L6 14Z\" fill=\"#000000\"/>\r\n"
+"</svg>";
+
+const char* reverse_svg = (const char*) temp_binary_data_4;
+
+//================== swap.svg ==================
+static const unsigned char temp_binary_data_5[] =
+"<?xml version=\"1.0\" encoding=\"utf-8\"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->\r\n"
+"<svg width=\"800px\" height=\"800px\" viewBox=\"0 0 16 16\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\r\n"
+"<path d=\"M5 16V13L16 13V11L5 11L5 8H4L3.93403e-07 12L4 16H5Z\" fill=\"#000000\"/>\r\n"
+"<path d=\"M11 8L11 5L8.74227e-08 5L0 3L11 3V4.37112e-08L12 0L16 4L12 8H11Z\" fill=\"#000000\"/>\r\n"
+"</svg>";
+
+const char* swap_svg = (const char*) temp_binary_data_5;
+
+
+const char* getNamedResource (const char* resourceNameUTF8, int& numBytes);
+const char* getNamedResource (const char* resourceNameUTF8, int& numBytes)
+{
+    unsigned int hash = 0;
+
+    if (resourceNameUTF8 != nullptr)
+        while (*resourceNameUTF8 != 0)
+            hash = 31 * hash + (unsigned int) *resourceNameUTF8++;
+
+    switch (hash)
+    {
+        case 0x6932a5d0:  numBytes = 464; return delete_svg;
+        case 0xb579bd30:  numBytes = 328; return duplicate_svg;
+        case 0x71d32550:  numBytes = 668; return lock_svg;
+        case 0xfd867762:  numBytes = 874; return regen_svg;
+        case 0x5ab2ff87:  numBytes = 306; return reverse_svg;
+        case 0xed09fdb8:  numBytes = 402; return swap_svg;
+        default: break;
+    }
+
+    numBytes = 0;
+    return nullptr;
+}
+
+const char* namedResourceList[] =
+{
+    "delete_svg",
+    "duplicate_svg",
+    "lock_svg",
+    "regen_svg",
+    "reverse_svg",
+    "swap_svg"
+};
+
+const char* originalFilenames[] =
+{
+    "delete.svg",
+    "duplicate.svg",
+    "lock.svg",
+    "regen.svg",
+    "reverse.svg",
+    "swap.svg"
+};
+
+const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
+const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8)
+{
+    for (unsigned int i = 0; i < (sizeof (namedResourceList) / sizeof (namedResourceList[0])); ++i)
+        if (strcmp (namedResourceList[i], resourceNameUTF8) == 0)
+            return originalFilenames[i];
+
+    return nullptr;
+}
+
+}
---
+++ JuceLibraryCode/BinaryData.h
@@ -0,0 +1,45 @@
+/* =========================================================================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+#pragma once
+
+namespace BinaryData
+{
+    extern const char*   delete_svg;
+    const int            delete_svgSize = 464;
+
+    extern const char*   duplicate_svg;
+    const int            duplicate_svgSize = 328;
+
+    extern const char*   lock_svg;
+    const int            lock_svgSize = 668;
+
+    extern const char*   regen_svg;
+    const int            regen_svgSize = 874;
+
+    extern const char*   reverse_svg;
+    const int            reverse_svgSize = 306;
+
+    extern const char*   swap_svg;
+    const int            swap_svgSize = 402;
+
+    // Number of elements in the namedResourceList and originalFileNames arrays.
+    const int namedResourceListSize = 6;
+
+    // Points to the start of a list of resource names.
+    extern const char* namedResourceList[];
+
+    // Points to the start of a list of resource filenames.
+    extern const char* originalFilenames[];
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding data and its size (or a null pointer if the name isn't found).
+    const char* getNamedResource (const char* resourceNameUTF8, int& dataSizeInBytes);
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding original, non-mangled filename (or a null pointer if the name isn't found).
+    const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
+}
